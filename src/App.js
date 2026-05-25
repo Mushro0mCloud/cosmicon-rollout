f983660f7a79cfa996f75f8fd847ffa4781fe0c1
Mushro0mCloud <jj.jeremychristian@gmail.com>
2026-05-25 20:20:06 +0700

    hard fix for vm cosmicon

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import PlayerPanel from './components/PlayerPanel'
 import GameOverModal from './components/GameOverModal';
 import MidTurnModal from './components/MidTurnModal';
 
-const API_URL = 'http://127.0.0.1:5000'
+const API_URL = 'http://35.255.251.132:5000'
 let socket = null;
 
 const getSocket = () => {
